7f1e9ac44f1cf762e7b90dffb8ee6a28cec0ab30
gsvijayragunath <gsvijayragunath@gmail.com>
2024-12-20 20:43:18 +0530

    location_change

--- server/server.js
+++ server/server.js
@@ -1,32 +1,25 @@
 exports = {
   onTicketCreateHandler: async function (args) {
+    console.log(args)
     try {
-      console.log("Ticket created by: " + args["data"]["requester"]["name"]);
-      console.log("Ticket Data:", args);
-
-      const values = await $request.invokeTemplate("getTickets", {});
-      console.log("Data obtained from getTickets:", values);
-
-      const { subject, description_text, status, priority } = args.data.ticket;
+      const { subject, description, status, priority,cc_emails } = args.data.ticket;
       const requesterEmail = args.data.requester.email;
 
       const payload = {
         email: requesterEmail,
         subject: subject,
-        description: description_text,
+        description: description,
         status: status,
         priority: priority,
+        cc_emails:cc_emails
       };
 
-      console.log("Payload constructed:", payload);
-
       const jsonPayload = JSON.stringify(payload);
-      console.log("JSONPAYLOAD", jsonPayload);
+      // console.log("JSONPAYLOAD", jsonPayload);
 
       const result = await $request.invokeTemplate("createFreshDeskTicket", {
         body: jsonPayload,
       });
-
       console.log("Ticket created successfully:", result);
     } catch (error) {
       console.error("Error during ticket creation:", error);
@@ -34,6 +27,7 @@ exports = {
   },
 
   onAppInstallHandler: function (args) {
+    generateTargetUrl()
     console.info("App installed with the following data:", args);
     renderData();
   },
